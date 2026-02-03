7efd64c16e98d7613dc25cfa7d3120c81019c8d9
Shogo <shogo.a.hardy@gmail.com>
2026-02-02 22:28:11 -0700

    Add A-Z keys

--- CodeType/Assets/Scripts/KeyAnimationObject.cs
+++ CodeType/Assets/Scripts/KeyAnimationObject.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class KeyAnimatedObject : MonoBehaviour
 {
     private Animator animator;
-    // Add this so you can type "OnQClick" or "OnWKey" in the Inspector for each button
     public string triggerName = "Play";
 
     void Awake()
